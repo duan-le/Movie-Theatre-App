9e078e0ba6fea21c8e90cf8bc2501a2c9782a5bf
duan-le <56768212+duan-le@users.noreply.github.com>
2020-11-25 13:24:09 -0700

    Add all required classes

---
+++ src/Controller/MovieTheatreApp.java
@@ -0,0 +1,10 @@
+package Controller;
+
+public class MovieTheatreApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the driver class!");
+	}
+
+}
