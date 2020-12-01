5cd3e65e4117735c48b68efa8a52555df1898e87
duan-le <56768212+duan-le@users.noreply.github.com>
2020-12-01 11:43:05 -0700

    Fix TicketReceipt table

--- mysqldb/db_ticketreceipt.sql
+++ mysqldb/db_ticketreceipt.sql
@@ -26,7 +26,6 @@ DROP TABLE IF EXISTS `ticketreceipt`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ticketreceipt` (
   `TicketNumber` int NOT NULL,
-  `cardNumber` int NOT NULL,
   PRIMARY KEY (`TicketNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-30 23:03:26
+-- Dump completed on 2020-12-01 11:41:14
